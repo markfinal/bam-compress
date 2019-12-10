79c421fc04eba371dfdffcfb892b250b9fed7a7e
Mark Final <markfinal@hotmail.com>
2019-12-10 21:49:52 +0000

    [packages,zlib,1.2.11] Update for latest SDK template

--- packages/zlib-1.2.11/bam/Scripts/zlibsdk.cs
+++ packages/zlib-1.2.11/bam/Scripts/zlibsdk.cs
@@ -33,24 +33,19 @@ namespace zlib
     class SDK :
         C.SDKTemplate
     {
-        private readonly Bam.Core.TokenizedStringArray headers = new Bam.Core.TokenizedStringArray();
+        private readonly Bam.Core.StringArray headers;
         private readonly Bam.Core.TypeArray libraryTypes = new Bam.Core.TypeArray(typeof(ZLib));
 
         public SDK()
         {
-            var headerPaths = new Bam.Core.StringArray
+            this.headers = new Bam.Core.StringArray
             {
                 "zlib.h",
                 "zconf.h"
             };
-
-            foreach (var header in headerPaths)
-            {
-                this.headers.Add(this.CreateTokenizedString("$(packagedir)/" + header));
-            }
         }
 
-        protected override Bam.Core.TokenizedStringArray HeaderFiles => this.headers;
+        protected override Bam.Core.StringArray HeaderFiles => this.headers;
         protected override Bam.Core.TypeArray LibraryModuleTypes => this.libraryTypes;
     }
 }
