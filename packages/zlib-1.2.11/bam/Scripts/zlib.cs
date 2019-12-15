8c60d22cdf938aa3b07dc05829a28d22a1c0aa9d
Mark Final <markfinal@hotmail.com>
2019-12-15 18:29:52 +0000

    [packages,zlib,1.2.11] Removed suppressions as no more warnings as errors

--- packages/zlib-1.2.11/bam/Scripts/zlib.cs
+++ packages/zlib-1.2.11/bam/Scripts/zlib.cs
@@ -61,49 +61,32 @@ namespace zlib
             this.CreateHeaderCollection("$(packagedir)/*.h");
             var source = this.CreateCSourceCollection("$(packagedir)/*.c");
 
-            if (source.Compiler is VisualCCommon.CompilerBase)
-            {
-                source.SuppressWarningsDelegate(new VisualC.WarningSuppression.Zlib());
-            }
-            else if (source.Compiler is GccCommon.CompilerBase)
-            {
-                source.SuppressWarningsDelegate(new Gcc.WarningSuppression.Zlib());
-            }
-            else if (source.Compiler is ClangCommon.CompilerBase)
-            {
-                source.SuppressWarningsDelegate(new Clang.WarningSuppression.Zlib());
-            }
-
             source.PrivatePatch(settings =>
                 {
                     var preprocessor = settings as C.ICommonPreprocessorSettings;
 
                     var cCompiler = settings as C.ICOnlyCompilerSettings;
-                    cCompiler.LanguageStandard = C.ELanguageStandard.C89;
+                    cCompiler.LanguageStandard = C.ELanguageStandard.C99;
 
                     if (settings is VisualCCommon.ICommonCompilerSettings visualCCompiler)
                     {
                         visualCCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
                         preprocessor.PreprocessorDefines.Add("_WINDOWS");
                         preprocessor.PreprocessorDefines.Add("ZLIB_DLL");
-
-                        var compiler = settings as C.ICommonCompilerSettings;
-                        compiler.DisableWarnings.AddUnique("4131"); // happens to a lot of files
                     }
-
                     if (settings is MingwCommon.ICommonCompilerSettings mingwCompiler)
                     {
                         mingwCompiler.AllWarnings = true;
                         mingwCompiler.ExtraWarnings = true;
                         mingwCompiler.Pedantic = true;
                     }
-
                     if (settings is GccCommon.ICommonCompilerSettings gccCompiler)
                     {
                         gccCompiler.AllWarnings = true;
                         gccCompiler.ExtraWarnings = true;
                         gccCompiler.Pedantic = true;
 
+                        /*
                         preprocessor.PreprocessorDefines.Add("HAVE_UNISTD_H"); // for lseek etc
 
                         // because this is not c99
@@ -112,34 +95,34 @@ namespace zlib
 
                         // flip the normal rules of visibility - make everything visible, and hide internals
                         preprocessor.PreprocessorDefines.Add("HAVE_HIDDEN");
+                        */
                         gccCompiler.Visibility = GccCommon.EVisibility.Default;
                     }
-
                     if (settings is ClangCommon.ICommonCompilerSettings clangCompiler)
                     {
                         clangCompiler.AllWarnings = true;
                         clangCompiler.ExtraWarnings = true;
                         clangCompiler.Pedantic = true;
-
+                        /*
                         preprocessor.PreprocessorDefines.Add("HAVE_UNISTD_H"); // for lseek etc
 
                         // flip the normal rules of visibility - make everything visible, and hide internals
                         preprocessor.PreprocessorDefines.Add("HAVE_HIDDEN");
                         clangCompiler.Visibility = ClangCommon.EVisibility.Default;
+                        */
                     }
                 });
 
-            if (null != this.WindowsVersionResource)
-            {
-                this.WindowsVersionResource.PrivatePatch(settings =>
+            this.WindowsVersionResource?.PrivatePatch(settings =>
+                {
+                    if (settings is MingwCommon.ICommonWinResourceCompilerSettings)
                     {
-                        if (settings is MingwCommon.ICommonWinResourceCompilerSettings)
+                        if (settings is C.ICommonWinResourceCompilerSettings rc)
                         {
-                            var rc = settings as C.ICommonWinResourceCompilerSettings;
                             rc.PreprocessorDefines.Add("GCC_WINDRES");
                         }
-                    });
-            }
+                    }
+                });
         }
     }
 }
